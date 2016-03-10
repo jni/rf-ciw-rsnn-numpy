1aa1319ca6c214ac03f395b2f31a51be3e9052b9
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-03-10 19:39:21 +1100

    Implement initial stages of training

--- rf-ciw-elm.py
+++ rf-ciw-elm.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-def train_rfciw(image_data, labels,
+eps = np.finfo(float).eps
+
+def train_rfciw(images, labels,
                 n_hidden=1600, input_weight_scaling=2, ridge=1e-8,
                 min_mask_size=10, rf_border=3):
     """Train the neural network, given input images and target labels.
@@ -18,12 +20,30 @@ def train_rfciw(image_data, labels,
     -------
 
     """
-    imsize = image_data.shape[1]
-    receptive_fields = np.zeros(n_hidden, imsize)
+
+    nimages = images.shape[0]
+    imsize = images.shape[1]
     sidelen = np.sqrt(imsize)
+    # generate random receptive fields
+    receptive_fields = np.zeros(n_hidden, imsize, dtype=bool)
     for i in range(n_hidden):
-        mask = np.zeros((sidelen, sidelen), dtype=bool)
+        mask = np.reshape(receptive_fields[i], (sidelen, sidelen))
         inds = np.zeros((2, 2))
         while np.prod(inds[1] - inds[0]) < min_mask_size:
             inds = np.sort(np.random.randint(rf_border, sidelen - rf_border,
                                              size=(2, 2)), axis=0)
+        rows, cols = inds.T
+        mask[rows[0]:rows[1], cols[0]:cols[1]] = True
+
+    # generate constrained weights
+    biases = np.zeros((n_hidden, 1), dtype=float)
+    w_random = np.zeros((n_hidden, imsize), dtype=float)
+    for i in range(n_hidden):
+        norm, n1, n2 = 0, 0, 0
+        while labels[n1] == labels[n2] or norm < eps:
+            n1, n2 = np.random.choice(nimages, size=2, replace=False)
+            wrow = receptive_fields[i] * (images[n1] - images[n2])
+            norm = np.linalg.norm(wrow)
+        w_random[i] = wrow / norm
+        biases[i] = 0.5 * (images[n1] + images[n2]) @ w_random[i]
+    w_random *= input_weight_scaling
