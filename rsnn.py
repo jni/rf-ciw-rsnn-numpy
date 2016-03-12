e14a48d255d97a49306e8c86e5ee632799e57b08
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-03-12 21:13:46 +1100

    Split code into more compact functions

--- rsnn.py
+++ rsnn.py
@@ -3,6 +3,99 @@ from skimage import segmentation as seg
 
 eps = np.finfo(float).eps
 
+
+def _generate_receptive_fields(n_hidden, image_side_length,
+                               min_mask_size, rf_border):
+    """Generate the random receptive fields.
+
+    Parameters
+    ----------
+    n_hidden : int
+        The number of hidden neurons.
+    image_side_length : int
+        The side length of an image
+    min_mask_size : int
+        The minimum area covered by a random receptive field.
+    rf_border : int
+        Don't set up receptive fields less than this many pixels away
+        from the image border.
+
+    Returns
+    -------
+    receptive_fields : array of float, shape (n_hidden, image_side_length ** 2)
+        The receptive fields.
+    """
+    sidelen = image_side_length
+    imsize = sidelen * sidelen
+    receptive_fields = np.zeros((n_hidden, imsize), dtype=bool)
+    for i in range(n_hidden):
+        mask = np.reshape(receptive_fields[i], (sidelen, sidelen))
+        inds = np.zeros((2, 2))
+        while np.prod(inds[1] - inds[0]) < min_mask_size:
+            inds = np.sort(np.random.randint(rf_border, sidelen - rf_border,
+                                             size=(2, 2)), axis=0)
+        rows, cols = inds.T
+        mask[rows[0]:rows[1], cols[0]:cols[1]] = True
+
+
+def _generate_random_weights(images, labels, receptive_fields,
+                             input_weight_scaling):
+    """Generate the random input neuron weights.
+
+    Parameters
+    ----------
+    images : array, shape (n_images, image_size)
+        The input images.
+    labels : array of int, shape (n_images)
+        The image labels.
+    receptive_fields : array of bool, shape (n_hidden, image_size)
+        The neuron receptive fields (the locations of pixels in the
+        image that each hidden neuron is sensitive to.
+    input_weight_scaling : float
+        Scale the random weights by this factor, before adding the bias.
+
+    Returns
+    -------
+    w_random : array of float, shape (n_hidden, imsize + 1)
+        The random weights, including an extra column to implement bias.
+    """
+    n_hidden, imsize = receptive_fields.shape
+    nimages = labels.shape[0]
+    biases = np.zeros((n_hidden, 1), dtype=float)
+    w_random = np.zeros((n_hidden, imsize), dtype=float)
+    for i in range(n_hidden):
+        norm, n1, n2 = 0, 0, 0
+        while labels[n1] == labels[n2] or norm < eps:
+            n1, n2 = np.random.choice(nimages, size=2, replace=False)
+            wrow = receptive_fields[i] * (images[n1] - images[n2])
+            norm = np.linalg.norm(wrow)
+        w_random[i] = wrow / norm
+        biases[i] = 0.5 * (images[n1] + images[n2]) @ w_random[i]
+    w_random *= input_weight_scaling
+    w_random = np.concatenate((w_random, biases), axis=1)
+    return w_random
+
+
+def one_hot(labels):
+    """One-hot encode sequential labels {0, 1, ..., m}.
+
+    Parameters
+    ----------
+    labels : array of int, shape (n,)
+        The input labels, encoded as sequential integers.
+
+    Returns
+    -------
+    encoded : array of int in {0, 1}, shape (n, m + 1)
+        The one-hot encoding of the input labels.
+    """
+    n = len(labels)
+    m = np.max(labels)
+    encoded = np.zeros((n, m + 1))
+    encoded[np.arange(n), labels] = 1
+    return encoded
+
+
 def train_rfciw(images, labels,
                 n_hidden=1600, input_weight_scaling=2, ridge=1e-8,
                 min_mask_size=10, rf_border=3):
@@ -40,37 +133,22 @@ def train_rfciw(images, labels,
     nimages = images.shape[0]
     imsize = images.shape[1]
     sidelen = np.sqrt(imsize)
-    # generate random receptive fields
-    receptive_fields = np.zeros((n_hidden, imsize), dtype=bool)
-    for i in range(n_hidden):
-        mask = np.reshape(receptive_fields[i], (sidelen, sidelen))
-        inds = np.zeros((2, 2))
-        while np.prod(inds[1] - inds[0]) < min_mask_size:
-            inds = np.sort(np.random.randint(rf_border, sidelen - rf_border,
-                                             size=(2, 2)), axis=0)
-        rows, cols = inds.T
-        mask[rows[0]:rows[1], cols[0]:cols[1]] = True
 
-    # generate constrained weights
-    biases = np.zeros((n_hidden, 1), dtype=float)
-    w_random = np.zeros((n_hidden, imsize), dtype=float)
-    for i in range(n_hidden):
-        norm, n1, n2 = 0, 0, 0
-        while labels[n1] == labels[n2] or norm < eps:
-            n1, n2 = np.random.choice(nimages, size=2, replace=False)
-            wrow = receptive_fields[i] * (images[n1] - images[n2])
-            norm = np.linalg.norm(wrow)
-        w_random[i] = wrow / norm
-        biases[i] = 0.5 * (images[n1] + images[n2]) @ w_random[i]
-    w_random *= input_weight_scaling
+    # generate the input weights matrix
+    receptive_fields = _generate_receptive_fields(n_hidden, sidelen,
+                                                  min_mask_size, rf_border)
+    w_random = _generate_random_weights(images, labels, receptive_fields,
+                                        input_weight_scaling)
 
+    # add bias column to the image data
     images = np.concatenate((images, np.ones((images.shape[0], 1))), axis=1)
-    w_random = np.concatenate((w_random, biases), axis=1)
 
     activations = 1 / (1 + np.exp(-w_random @ images.T))
 
     # generate targets matrix from labels (one-hot encoding)
     targets = one_hot(labels)
+
+    # solve for the output weights
     w_out = np.linalg.lstsq(activations @ activations.T +
                             ridge * np.ones((n_hidden, n_hidden)),
                             activations @ targets)[0]
@@ -87,14 +165,6 @@ def predict_class(images, w_random, w_out):
 def test_accuracy(images, labels, w_random, w_out):
     predicted_labels = predict_class(images, w_random, w_out)
     return np.mean(labels == predicted_labels)
-
-
-def one_hot(labels):
-    n = len(labels)
-    relab, fw, unique = seg.relabel_sequential(labels)
-    encoded = np.zeros((n, len(unique)))
-    encoded[np.arange(n), relab] = 1
-    return encoded
 
 
 if __name__ == '__main__':
